dde0777e79a5933bf0414d7d817ae13aec360ee8
masagus hariadi arief <adi.arief77@gmail.com>
2019-01-11 10:25:53 +0700

    Refactor content style into Page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,22 +4,15 @@ import { Button } from "reactstrap";
 
 import Page from "../Page";
 
-const mainStyle = {
-  color: "red",
-  backgroud: "black"
-};
-
 const Paragraph = styled.p`
   font-size: 20px;
 `;
 
 const Content = () => (
   <Page>
-    <main style={mainStyle}>
-      <Paragraph>this component made in class 'content'</Paragraph>
-      <Button color="primary">Get Srarted</Button>
-      <Button color="danger">Danger</Button>
-    </main>
+    <Paragraph>this component made in class 'content'</Paragraph>
+    <Button color="primary">Get Srarted</Button>
+    <Button color="danger">Danger</Button>
   </Page>
 );
 
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -3,10 +3,16 @@ import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 
+const mainStyle = {
+  color: "yellow",
+  background: "black",
+  padding: "10px"
+};
+
 const Page = Props => (
   <div>
     <Header />
-    {Props.children}
+    <main style={mainStyle}>{Props.children}</main>
     <Footer />
   </div>
 );
